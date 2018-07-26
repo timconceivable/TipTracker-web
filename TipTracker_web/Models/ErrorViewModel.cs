13bc1c1690c38dff42450fe07c8b036719cc1c12
Tim Conceivable <headskape@yahoo.com>
2018-07-25 22:29:23 -0500

    Add project files.

---
+++ TipTracker_web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TipTracker_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
